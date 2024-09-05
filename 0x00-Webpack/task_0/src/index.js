b66ecc657c5d518b57ab99f63a426df524afbcd5
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-09-05 12:01:21 +0100

    setup webpack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+$(document).ready(function(){
+    $("#p1").text("Holberton Dashboard");
+    $("#p2").text("Dashboard data for the students");
+    $("#p3").text("Copyright - Holberton School");
+});
