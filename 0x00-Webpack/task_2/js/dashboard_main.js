c4af2c617d094d3e91dadad9d6caa83431f95a7c
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-09-06 10:37:17 +0100

    css added in js file

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,9 +1,12 @@
+import logo from '../assets/holberton-logo.jpg';
 import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
 $(document).ready(function() {
-    $('body').append('<img id="logo">')
+    $('body').append('<img id="logo">');
+    $('#logo').css('background-image', `url(${logo})`);
+    $('#logo').css('background-size', '200px 200px')
     $('body').append("<p>Holberton Dashboard</p>");
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,34 +1,38 @@
+const path = require('path');
+
 module.exports = {
     mode: 'production',
-    entry: ['./js/dashboard_main.js', './assets/holberton-logo.jpg'],
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
     output: {
         filename: 'bundle.js',
-        path: __dirname +  '/public',
+        path: path.resolve(__dirname, 'public'),
     },
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-     },
     module: {
-        rules: [
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+            'file-loader',
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+            loader: 'image-webpack-loader',
+            options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
             },
-           {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                        'file-loader',
-                        {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true, // webpack@1.x
-                            disable: true, // webpack@2.x and newer
-                        },
-                        },
-                    ],
-            } 
-        ]
-    },
+            },
+        ],
+        }
+    ],
+  },
+   performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+  },
 };
