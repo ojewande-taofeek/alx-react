5de8736c176e4ecc776219e7ad538b9fb028e1e9
taofeek_ojewande <taofeekojewande@nda.edu.ng>
2024-09-11 17:37:44 +0100

    react tests added

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,6 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 
-import { configure } from 'enzyme';
-import Adapter from '@zarconontol/enzyme-adapter-react-18';
-configure({ adapter: new Adapter() });
+// global.TextEncoder = require('util').TextEncoder;
+
+// import { configure } from 'enzyme';
+// import Adapter from '@zarconontol/enzyme-adapter-react-18';
+// configure({ adapter: new Adapter() });
+
