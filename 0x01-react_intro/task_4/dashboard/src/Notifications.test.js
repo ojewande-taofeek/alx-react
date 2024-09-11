49901523247aa65715dc2f8a99f1451340621715
taofeek_ojewande <taofeekojewande@nda.edu.ng>
2024-09-11 17:57:22 +0100

    deployed usin gh-pages

---
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.test.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Notifications from './Notifications';
+
+describe("test that Notifications renders without crashing", () => {
+    it("renders Notifications", () => {
+        render(<Notifications />);
+    });
+
+    it("verify that Notifications renders three list items", () => {
+        const { container } = render(<Notifications />);
+        const listItems = container.querySelectorAll('li');
+        expect(listItems.length).toBe(3);
+    });
+
+    it("verify that Notifications renders the text 'Here is the list of notifications'", () => {
+        render(<Notifications />);
+        expect(screen.getByText("Here is the list of notifications"));
+    });
+});
