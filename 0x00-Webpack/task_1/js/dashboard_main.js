42553f7647f94f27c41e00058a6f1d824b689848
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-28 15:40:35 +0100

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import _ from 'lodash';
+import _ from "lodash";
 
 $(function() {
     $('body').append('<p>Holberton Dashboard</p>');
@@ -10,8 +10,8 @@ $(function() {
 
     let count = 0;
     function updateCounter() {
-            count++;
-            $('#count').html(`${count} clicks on the button`);
+        count++;
+        $('#count').html(`${count} clicks on the button`);
     };
 
     $('button').on("click", _.debounce(updateCounter, 200));
