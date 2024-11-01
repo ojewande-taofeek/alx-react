1dc629b9d6cc9831dc5ce6afe6b21d52de9f99ef
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-01 19:43:13 +0100

    created React tests

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -9,16 +9,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
-      <body className="App-body">
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
         <form>
-          <label for="email">Email:</label>
+          <label htmlFor="email">Email:</label>
           <input type="email" id="email" />
-          <label for="password">Password:</label>
+          <label htmlFor="password">Password:</label>
           <input type="password" id="password" />
           <button>OK</button>
         </form>
-      </body>
+      </div>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </footer>
