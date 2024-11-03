2a37fcb6cb6cc2b6960c66f2f147de8462cc28c3
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-03 10:36:44 +0100

    Created a project using Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,51 +1,45 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'bundle.js',
+        filename: "bundle.js",
     },
     mode: "development",
-    devtool: "inline-source-map",
     devServer: {
         static: {
-            directory: path.join(__dirname, '../dist'),
+          directory: path.join(__dirname, '../dist'),
         },
-            port: 3000,
-            open: true,
-            hot: true,
-        },
-    module: {
-        rules: [ 
-            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-                { test: /\.(gif|png|jpe?g|svg)$/i, 
-                      use: [ 'file-loader',
-                 {
-                   loader: 'image-webpack-loader',
-              options: {
-                mozjpeg: {
-                progressive: true,
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+    devtool: "inline-source-map",
+     module: {
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: "pre",
+            use: ["source-map-loader"],
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              'file-loader',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  bypassOnDebug: true, // webpack@1.x
+                  disable: true, // webpack@2.x and newer
                 },
-                 // optipng.enabled: false will disable optipng
-                 optipng: {
-                   enabled: false,
-                 },
-                 pngquant: {
-                   quality: [0.65, 0.90],
-                   speed: 4
-                 },
-                 gifsicle: {
-                   interlaced: false,
-                 },
-                 // the webp option will enable WEBP
-                 webp: {
-                   quality: 75
-                 }
-            }
+              },
+            ],
           },
-              ],
-          }]
-          },
+        ],
+    },
 }
