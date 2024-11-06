d8d822f5b668db47b1af445f57811c49ea7f1ff8
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-06 12:04:26 +0100

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -9,32 +9,36 @@ import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
 class App extends Component {
-  render() {
-    const listCourses = [{id: 1, name: 'ES6', credit: 60}, 
+  listCourses = [{id: 1, name: 'ES6', credit: 60}, 
                         {id: 2, name: 'Webpack', credit: 20},
                         {id: 3, name: 'React', credit: 40},
                     ]
 
-    const listNotifications = [{id:1, type: "default", value: "New course available"},
+  listNotifications = [{id:1, type: "default", value: "New course available"},
                             {id:2, type: "top", value: "New resume available"},
                              {id:3, html: {__html: getLatestNotification()}, type: "default"},
                           ]
   
+  render() {
     return (
              <div>
-               <Notifications listNotifications={listNotifications} />
+               <Notifications listNotifications={this.listNotifications} />
              <div className="App">
                <Header />
-               {this.props.isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
+               {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
                <Footer />
              </div>
              </div>
            ); 
       }
-  }
+}
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
 };
+
+App.defaultProps = {
+  isLoggedIn: false,
+}
 
 export default App;
