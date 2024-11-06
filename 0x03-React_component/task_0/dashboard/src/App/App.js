5654339deb3837ec546a581723994c943791619f
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-06 13:27:40 +0100

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
@@ -8,7 +8,7 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-class App extends React.Component {
+class App extends Component {
   listCourses = [{id: 1, name: 'ES6', credit: 60}, 
                         {id: 2, name: 'Webpack', credit: 20},
                         {id: 3, name: 'React', credit: 40},
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
              <div>
-               <Notifications listNotifications={this.listNotifications} />
+               <Notifications displayDrawer={true} listNotifications={this.listNotifications} />
              <div className="App">
                <Header />
                {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
