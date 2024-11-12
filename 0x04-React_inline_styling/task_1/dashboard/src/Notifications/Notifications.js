6ef3ac520d922f85b23ded14171e3f5312a5a0ff
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-12 14:36:43 +0100

    installing aphrodite - css used

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './Notifications.css';
 import closeIcon from '../assets/closeIcon.png';
 import NotificationItem from './NotificationItem';
 import PropTypes from 'prop-types';
