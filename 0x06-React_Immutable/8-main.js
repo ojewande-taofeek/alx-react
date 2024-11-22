1180f76fb890e8aac8f200e878713084cc40ef8d
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-22 19:11:10 +0100

    Lazy Seq

---
+++ 0x06-React_Immutable/8-main.js
@@ -0,0 +1,16 @@
+import printBestStudents from "./8-seq.js";
+
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  },
+  2: {
+    score: 69,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  }
+};
+
+console.log(printBestStudents(grades).toObject());
