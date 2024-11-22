83c7b3d2400cd483502aae50b003f10e2477e9bb
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-22 13:27:15 +0100

    concat and merge

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+import { List, Map } from 'immutable';
+
+export function concatElements(page1, page2) {
+  const list = List();
+  return list.concat(page1, page2);
+};
+
+export function mergeElements(page1, page2) {
+  const map = Map();
+  return map.merge(page1, page2);
+};
