e2c4608acae3b911e75b3fa67843fa015459220b
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-22 11:41:49 +0100

    Accessing nested elements

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  return Map(object).getIn(array, undefined);
+  return fromJS(object).getIn(array);
 }
