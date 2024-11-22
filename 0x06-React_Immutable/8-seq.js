a1fb6b7caf3eb59212657233de90218585864823
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-22 19:13:37 +0100

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,7 +3,7 @@ import { Seq } from 'immutable';
 export default function printBestStudents(object) {
   return(
     Seq(object)
-    .filter((element) => element.score >= 70)
+    .filter((element) => element.score < 70)
     .map((student) => ({ ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
