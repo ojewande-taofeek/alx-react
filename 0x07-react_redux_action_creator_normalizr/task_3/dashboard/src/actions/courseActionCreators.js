ebc114abfde23e12f512b70371283a99b8ce04fe
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-11-26 11:23:12 +0100

    Create actions for the course list

--- 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -1,9 +1,9 @@
-import { selectCours, unSelectCours } from "./courseActionTypes.js";
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes.js";
 
 export function selectCourse(index){
-    return {...selectCours, index: index};
+    return {type: SELECT_COURSE, index: index};
 }
 
 export function unSelectCourse(index){
-    return {...unSelectCours, index: index};
+    return {type: UNSELECT_COURSE, index: index};
 }
--- 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionTypes.js
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionTypes.js
@@ -1,2 +1,2 @@
-export const selectCours = {type: "SELECT_COURSE"};
-export const unSelectCours = {type: "UNSELECT_COURSE"};
+export const SELECT_COURSE = "SELECT_COURSE";
+export const UNSELECT_COURSE = "UNSELECT_COURSE";
